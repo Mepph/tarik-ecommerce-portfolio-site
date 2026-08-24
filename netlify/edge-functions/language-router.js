85dc2ea1ffddb12c58c690c5c0a49ee8324cba1d
Mepph <qazwsx123ta@gmail.com>
2026-08-25 00:29:34 +0300

    Serve the English portfolio to all visitors

--- netlify/edge-functions/language-router.js
+++ netlify/edge-functions/language-router.js
@@ -1,14 +1,4 @@
-export default function languageRouter(request, context) {
-  const url = new URL(request.url);
-  const requestedLanguage = url.searchParams.get("lang");
-  const language = requestedLanguage === "tr" || requestedLanguage === "en"
-    ? requestedLanguage
-    : context.geo?.country?.code === "TR"
-      ? "tr"
-      : "en";
-
-  url.pathname = `/${language}/`;
-  return url;
+export default function languageRouter(_request, context) {
+  return context.next();
 }
-
 export const config = { path: "/" };
